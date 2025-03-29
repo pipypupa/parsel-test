41d45d1e4b072288c15d77809d76dd4e4bd360ca
pipypupa <zong8729@gmail.com>
2025-03-29 19:04:54 +0200

    add script

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import "./js/minus"
-import "./js/multiplies"
+import "./js/minus";
+import "./js/multiplies";
 
-
-import {addNumbers} from "./js/add"
-const numbers = addNumbers(2, 4)
-console.log(numbers)
+import { addNumbers } from "./js/add";
+const numbers = addNumbers(2, 4);
+console.log(numbers);
